8d81941111b4784bfd03dcf70e9735031d9f58bb
Nzogue Dibiye <nzogue.dibiye@gmail.com>
2025-01-29 09:20:33 +0100

    version 1.2

--- frontend/src/components/Evenements.jsx
+++ frontend/src/components/Evenements.jsx
@@ -1,57 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const EventsSection = () => {
   const events = [
     {
       id: 1,
-      image: "../../images/livre1.png", // Remplacez par le chemin de votre image
+      image: "../../images/livre1.png",
       title: "Manuel d'initiation",
       description:
-        "dsdkjghsdgj sdjshfkshdksdg sqdasf qsfjgf qsfhqsfgqh sqfshqfh sqfhqsq qsfhqsfhsf",
+        "Ce manuel d'initiation est destiné aux étudiants débutants et couvre les bases essentielles de l'informatique. Apprenez à utiliser divers outils numériques, à résoudre des problèmes courants et à construire une base solide pour avancer dans le monde technologique. Ce manuel comprend des exercices pratiques, des études de cas et des démonstrations pour assurer une meilleure compréhension des concepts.",
     },
     {
       id: 2,
-      image: "../../images/livre2.png", // Remplacez par le chemin de votre image
-      title: "Manuel d'initiation",
+      image: "../../images/livre2.png",
+      title: "Atelier de formation",
       description:
-        "dsdkjghsdgj sdjshfkshdksdg sqdasf qsfjgf qsfhqsfgqh sqfshqfh sqfhqsq qsfhqsfhsf",
+        "Cet atelier interactif propose des sessions de formation en groupe où les participants apprennent des compétences techniques avancées. Les thèmes abordés incluent la programmation, le développement d'applications, et l'analyse des systèmes. Profitez d'une expérience immersive avec des experts du domaine, qui partageront leurs connaissances et répondront à vos questions en temps réel.",
     },
     {
       id: 3,
-      image: "../../images/cat2.png", // Remplacez par le chemin de votre image
-      title: "Manuel d'initiation",
+      image: "../../images/cat2.png",
+      title: "Conférence annuelle",
       description:
-        "dsdkjghsdgj sdjshfkshdksdg sqdasf qsfjgf qsfhqsfgqh sqfshqfh sqfhqsq qsfhqsfhsf",
+        "La conférence annuelle rassemble les leaders de l'industrie, les enseignants, et les étudiants pour discuter des dernières tendances et innovations en matière d'éducation technologique. Participez à des tables rondes, écoutez des présentations inspirantes et découvrez les technologies qui façonnent le futur de l'apprentissage.",
     },
   ];
 
   return (
-    <div className="container mx-auto px-4 md:px-8 py-16">
+    <div className="container mx-auto px-10 md:px-5 py-5 text-center">
       {/* Titre de la section */}
-      <h2 className="text-[#C00000] text-2xl font-semibold mb-8 text-center">
+      <h2 className="text-[#C00000] text-2xl font-bold mb-8 text-left py-5">
         Actualité et événements
       </h2>
 
       {/* Cartes des événements */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {events.map((event) => (
           <div
             key={event.id}
-            className="bg-white shadow-xl rounded-lg overflow-hidden flex flex-col justify-between h-[500px]" // Augmenter la longueur des div
+            className="bg-white drop-shadow-[0_4px_8px_rgba(0,0,0,0.5)]  overflow-hidden flex flex-col justify-between h-[600px] lg:w-[460px]" // Ombre au bord + Longueur augmentée
           >
             {/* Image */}
             <img
               src={event.image}
               alt={event.title}
-              className="object-cover w-full h-[250px]" // Ajuster la hauteur de l'image
+              className="object-cover w-full h-[250px]" // Hauteur augmentée pour l'image
             />
 
             {/* Contenu texte */}
-            <div className="p-4 flex-1">
-              <h3 className="text-gray-800 font-bold text-lg mb-2">
+            <div className="p-6 flex-1">
+              <h3 className="text-black font-bold text-xl mb-4">
                 {event.title}
               </h3>
-              <p className="text-gray-700 text-sm">{event.description}</p>
+              <p className="text-gray-700 text-sm leading-relaxed">
+                {event.description}
+              </p>
             </div>
 
             {/* Barre rouge en bas */}
--- frontend/src/components/Tendance/Avis/Avis.jsx
+++ frontend/src/components/Tendance/Avis/Avis.jsx
@@ -1,9 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import axios from 'axios';
-import quotes from '../../../../images/quotes.png';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Avis = () => {
   const [testimonials, setTestimonials] = useState([]);
@@ -13,7 +9,9 @@ const Avis = () => {
   useEffect(() => {
     const fetchTestimonials = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/concours/testimonials/');
+        const response = await axios.get(
+          "http://localhost:8000/concours/testimonials/"
+        );
         setTestimonials(response.data);
       } catch (err) {
         setError(err);
@@ -27,105 +25,55 @@ const Avis = () => {
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
-
-  const settings = {
-    dots: true,
-    arrows: false,
-    infinite: false,
-    className: "center",
-    speed: 800,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    cssEase: "ease-in-out",
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-          autoplay: true,
-          pauseOnHover: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          infinite: true,
-          arrows: false,
-          dots: true,
-          autoplay: true,
-          pauseOnHover: true,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          infinite: true,
-          arrows: false,
-          dots: true,
-          autoplay: true,
-          pauseOnHover: true,
-        },
-      },
-    ],
-  };
-
-  const truncateContent = (content, wordLimit) => {
-    const words = content.split(' ');
-    if (words.length <= wordLimit) return content;
-    return words.slice(0, wordLimit).join(' ') + '...';
-  };
-
+  
   return (
-    <div className="w-full m-auto flex flex-col justify-center items-center container mx-auto px-10 md:px-5 pb-10 md:pb-20">
-      <div className="text-center my-8">
-        <h1 className="text-lg md:text-xl lg:text-3xl font-bold text-[#2278AC]">Avis des utilisateurs</h1>
+    <div
+      className="relative bg-cover bg-center py-10 mb-20"
+      style={{
+        backgroundImage: `url('../../images/young.jpg')`, // Image d'arrière-plan
+      }}
+    >
+      {/* Titre */}
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-bold text-white">Témoignage</h1>
+        <div className="mt-4">
+          <hr className="border-t-2 border-white w-24 mx-auto" />
+        </div>
       </div>
-      <div className="container mx-auto px-10 md:px-5 h-auto lg:h-[500px]">
-        <Slider {...settings}>
-          {testimonials.map((testimonial) => (
-            <div key={testimonial.id} className="text-black flex flex-col px-4 py-4">
-              <div
-                className="rounded-lg shadow-lg p-6 max-w-[360px] md:max-w-[400px] lg:max-w-[450px] lg:ml-10 hover:scale-105 transition-transform duration-300 ease-in-out h-[400px]"
-                style={{
-                  backgroundColor: 'rgba(34, 120, 172, 0.7)', // Fond avec opacité
-                }}
-              >
-                <div className="flex justify-between items-center gap-2">
-                  <img
-                    src={testimonial.image}
-                    alt="avatar"
-                    className="w-20 h-20 rounded-full border-2 border-[#2278AC]"
-                  />
-                  <img src={quotes} alt="quotes icon" className="w-6 h-6" />
-                </div>
-                <div className="flex flex-col gap-4 h-full">
-                  <div className="my-6">
-                    <p className="pt-5 text-sm md:text-md lg:text-lg mb-3">
-                      {truncateContent(testimonial.content, 150)}
-                    </p>
-                  </div>
-                  <div className="flex justify-between items-center mt-auto">
-                    <h1 className="font-bold text-white text-md lg:text-xl">
-                      {testimonial.name}
-                    </h1>
-                    <p className="text-sm text-gray-200">
-                      {new Date(testimonial.date).toLocaleDateString()}
-                    </p>
-                  </div>
-                </div>
-              </div>
+
+      {/* Témoignages */}
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 px-4">
+        {testimonials.map((testimonial) => (
+          <div
+            key={testimonial.id}
+            className="bg-white shadow-lg rounded-xl overflow-hidden relative pt-16 pb-8 text-center"
+          >
+            {/* Avatar au-dessus de la carte */}
+            <div className="absolute -top-0 left-1/2 transform -translate-x-1/2 z-20">
+              <img
+                src={testimonial.image}
+                alt="Avatar"
+                className="w-20 h-20 rounded-full border-4 border-white shadow-lg object-cover"
+              />
             </div>
-          ))}
-        </Slider>
+
+            {/* Citation */}
+            <div className="px-6 mt-6">
+              <p className="text-orange-500 text-4xl font-serif mb-4">“</p>
+              <p className="text-gray-600 text-sm leading-relaxed">
+                {testimonial.content}
+              </p>
+            </div>
+
+            {/* Informations utilisateur */}
+            <div className="mt-6">
+              <h3 className="text-lg font-semibold text-gray-800">
+                {testimonial.name}
+              </h3>
+              <p className="text-sm text-gray-500">{testimonial.role}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
